154bd761acdbfb2a48da1fee31814086110a0ef6
Alsu <alsousha@mail.ru>
2020-12-09 12:18:19 +0200

    added tests -> success

--- src/test/java/ru/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/statistic/StatisticsServiceTest.java
@@ -1,16 +1,39 @@
 package ru.statistic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
-
+    StatisticsService service = new StatisticsService();
+    //@ParameterizedTest
+    //@CsvFileSource(resources = "/data.csv")
+//    void shouldCalculate(String test, long amount, boolean registered, long expected) {
+//
+//        BonusService service = new BonusService();
+//        long actual = service.calculate(amount, registered);
+//        assertEquals(expected, actual);
+//    }
     @Test
-    void findMax() {
-        StatisticsService service = new StatisticsService();
+    void findMaxIsBegin() {
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIsMiddle() {
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 13, 8, 6, 11, 11, 12};
+        long expected = 13;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIsEnd() {
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 22};
+        long expected = 22;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
